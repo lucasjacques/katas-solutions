7ac9463229d594094b7ae1cd27b77772f9808b71
Lucas Jacques <lucas.jacs@gmail.com>
2020-03-05 14:56:54 -0300

    Added Pascal triangle diagonal kata (doing)

---
+++ diagonal_pascal_triangle.js
@@ -0,0 +1,17 @@
+const diagonal = (n, p) => {
+	if (p === 0) {
+		return n+1;
+	}
+
+	const diagonals = [Array(n+1).fill(1)];
+	for (let i = 1; i <= p; i++) {
+		diagonals[i] = nextDiagonal(diagonals[0]);
+	}
+}
+
+const nextDiagonal = (previousDiagonal) => {
+	console.log(previousDiagonal);
+	const  = [];
+}
+
+console.log(diagonal(7, 1))
