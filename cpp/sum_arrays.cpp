0b064e26b90b47f1e44999d85a2de460c058f665
Lucas Jacques <lucas.jacs@gmail.com>
2022-09-14 13:17:33 -0300

    Improve previous C++ exercise to print in console

--- cpp/sum_arrays.cpp
+++ cpp/sum_arrays.cpp
@@ -1,4 +1,8 @@
 #include <vector>
+#include <string>
+#include <iostream>
+
+using vec = std::vector<int>;
 
 int sum(std::vector<int> nums) {
   int result = 0;
@@ -7,4 +11,13 @@ int sum(std::vector<int> nums) {
     result += nums[i];
   }
   return result;
+}
+
+int main () {
+  std::vector<int> example = vec{1, 2 , 3, 4};
+
+  int result = sum(example);
+
+  std::cout << result;
+  return 0;
 }
